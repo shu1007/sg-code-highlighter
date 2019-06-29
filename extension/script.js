22e46eaafd331711ae621c3efcb30274f4431ba9
NARIGASAWA Shu <narigasawa_shu@interprism.co.jp>
2019-06-30 03:05:14 +0900

    言語指定できるように改良

--- extension/script.js
+++ extension/script.js
@@ -1,54 +1,76 @@
-let prettyPrint = function() {
-  const CHECKED_CLASS_NAME = "checked";
-  const MESSAGE_CLASS_NAME = "message";
-  const CHAT_CLASS_NAME = "chat";
-  const PRETTYPRINT_CLASS_NAME = "prettyprint";
-  const LINENUMS_CLASS_NAME = "linenums";
-  const CLASS_ATTRIBUTE = "class";
+let appendPre = (chat, classes, text) => {
+  let pre = document.createElement("pre");
+  pre.setAttribute(CLASS_ATTRIBUTE, classes.join(" "));
+  pre.textContent = text;
+  chat.append(pre);
+};
 
+let prettyPrint = () => {
+  let regex = RegExp(REGEX_PATTERN, "gs");
   // DOM書き換え
-  document.querySelectorAll("." + CHAT_CLASS_NAME).forEach(chat => {
-    if (chat.classList.contains(CHECKED_CLASS_NAME)) return;
+  document
+    .querySelectorAll(`.${CHAT_CLASS_NAME}:not(.${CHECKED_CLASS_NAME})`)
+    .forEach(chat => {
+      chat.setAttribute(
+        CLASS_ATTRIBUTE,
+        [CHAT_CLASS_NAME, CHECKED_CLASS_NAME].join(" ")
+      );
 
-    chat.setAttribute(
-      CLASS_ATTRIBUTE,
-      [CHAT_CLASS_NAME, CHECKED_CLASS_NAME].join(" ")
-    );
-    let split = chat.innerText.split("```");
-    let length = split.length;
-    if (length < 2 || length % 2 == 0) return;
+      let index = 0;
+      let regArray;
+      let innerText = chat.innerText;
+      chat.innerHTML = "";
 
-    chat.innerHTML = "";
-    for (let i = 0; i < length; i++) {
-      if (split[i] === "") continue;
-
-      let pre = document.createElement("pre");
-      if (i % 2 == 1) {
-        pre.setAttribute(
-          CLASS_ATTRIBUTE,
-          [
-            MESSAGE_CLASS_NAME,
-            PRETTYPRINT_CLASS_NAME,
-            // 行番号いらない場合は下記のクラスは付けない
-            LINENUMS_CLASS_NAME
-          ].join(" ")
-        );
-      } else {
-        pre.setAttribute(CLASS_ATTRIBUTE, MESSAGE_CLASS_NAME);
+      while ((regArray = regex.exec(innerText)) !== null) {
+        let startIndex = regArray.index;
+        if (index <= startIndex - 1) {
+          //通常テキスト処理
+          appendPre(
+            chat,
+            [MESSAGE_CLASS_NAME],
+            innerText.substring(index, startIndex)
+          );
+        }
+        let foundStr = regArray[0];
+        let option = foundStr.match(/\`\`\`(.*?)\n/)[1].toLowerCase();
+        let classes = [
+          MESSAGE_CLASS_NAME,
+          PRETTYPRINT_CLASS_NAME,
+          LINENUMS_CLASS_NAME
+        ];
+        if (EXTENSIONS_SET.has(option)) {
+          classes = classes.concat(`lang-${option}`);
+        }
+        appendPre(chat, classes, foundStr.match(/\`\`\`.*?\n(.+?)\`\`\`/s)[1]);
+        index = regex.lastIndex;
       }
-      pre.textContent = split[i].trim();
-      chat.append(pre);
-    }
-  });
+
+      let textLength = innerText.length;
+      if (index < textLength - 1) {
+        appendPre(
+          chat,
+          [MESSAGE_CLASS_NAME],
+          innerText.substring(index, textLength)
+        );
+      }
+    });
 
   // 構文ハイライトを実行
   PR.prettyPrint();
 };
 
 let talkAreaObserver = new MutationObserver(prettyPrint);
-let observeTalkArea = function() {
+let observeTalkArea = () => {
   talkAreaObserver.observe(document.getElementById("talkArea"), {
     childList: true
+  });
+};
+
+let addSendMessageEvent = () => {
+  document.getElementById("message").addEventListener("keydown", event => {
+    if (event.metaKey && event.keyCode === ENTER_KEY_CODE) {
+      document.getElementsByClassName("mid-button")[0].click();
+    }
   });
 };
 
@@ -59,17 +81,12 @@ new MutationObserver(() => {
 
   prettyPrint();
   observeTalkArea();
+  addSendMessageEvent();
 }).observe(document.getElementById("content_panel"), {
   attributes: true
-});
-
-// metaKey + enterで送信可能にする
-document.getElementById("message").addEventListener("keydown", event => {
-  if (event.metaKey && event.keyCode == 13) {
-    document.getElementsByClassName("mid-button")[0].click();
-  }
 });
 
 //初期ロード時に実行
 prettyPrint();
 observeTalkArea();
+addSendMessageEvent();
